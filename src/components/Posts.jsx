c3da470368b6bcd1ceb16913421c1e4419b07808
Matias <mati.delarosa1984@gmail.com>
2024-06-09 00:23:45 -0300

    omplete the post card of the feed section

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,26 @@
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { app } from '../firebase';
+import Post from './Post';
 
-
-export default function Posts() {
+export default async function Posts() {
+  const db = getFirestore(app);
+  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
   return (
-    <div>Posts</div>
-  )
+    <div>
+      {data.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
